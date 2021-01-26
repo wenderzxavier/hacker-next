17e97dd41b4c4fa80ec38294de166eccc995cb9e
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-01-25 21:01:28 -0300

    add logic to change page based on initial props

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 import Error from "next/error";
+import Link from "next/link";
 import React from "react";
+import Layout from "../components/Layout";
 import StoryList from "../components/StoryList";
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories;
+    let page;
+
     try {
+      page = Number(query.page) || 1;
       const response = await axios.get(
-        "https://node-hnapi.herokuapp.com/news?page=1"
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
       );
       stories = await response.data;
     } catch (error) {
@@ -16,21 +21,39 @@ class Index extends React.Component {
       stories = [];
     }
 
-    return { stories };
+    return { stories, page };
   }
 
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
 
     if (stories.length === 0) {
       return <Error statusCode={503} />;
     }
 
     return (
-      <div>
-        <h1>Hacker News</h1>
+      <Layout
+        title="Hacker News"
+        description="A Hacker News clone made with Next.js"
+      >
         <StoryList stories={stories} />
-      </div>
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
+      </Layout>
     );
   }
 }
